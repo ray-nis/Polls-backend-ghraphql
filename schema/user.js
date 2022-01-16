4e86c8733207e399e0168f72327b301d6ca75836
ray <reirei256@gmail.com>
2022-01-17 00:42:22 +0100

    Graphql get users, user with id

--- schema/user.js
+++ schema/user.js
@@ -1,4 +1,9 @@
 const typeDef = `
+    extend type Query {
+        users: [User!]!
+        user(id: Int!): User
+    }
+
     type User {
         id: ID!
         username: String!
@@ -6,6 +11,18 @@ const typeDef = `
 `
 
 const resolvers = {
+    Query: {
+        users: async (parent, args, context) => {
+            return await context.db.User.findAll()
+        },
+        user: async (parent, args, context) => {
+            const user = await context.db.User.findByPk(args.id)
+            if (user === null) {
+                throw new Error(`User with id ${args.id} is not found`)
+            }
+            return user
+        }
+    }
 }
 
 module.exports = { typeDef, resolvers }
