58668fd9d6d7bf2e4bfc68c9ba68770a3a80f3f6
ray <reirei256@gmail.com>
2022-01-17 22:21:21 +0100

    Vote on poll

--- schema/pollOption.js
+++ schema/pollOption.js
@@ -4,9 +4,26 @@ const typeDef = `
         text: String!
         votes: Int!
     }
+
+    extend type Mutation {
+        vote(optionId: Int!): Poll!
+    }
 `
 
 const resolvers = {
+    Mutation: {
+        vote: async (parent, args, context) => {
+            const option = await context.db.PollOption.findByPk(args.optionId)
+            if (option === null) {
+                throw new Error("Not found")
+            }
+
+            option.votes = option.votes + 1
+            await option.save()
+
+            return await context.db.Poll.findByPk(option.pollId)
+        }
+    }
 }
 
 module.exports = { typeDef, resolvers }
